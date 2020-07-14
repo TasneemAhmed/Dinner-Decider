3fd393e3619dbd3fa5ed543c0c8b175e2bd9a895
TasneemAhmed <49993791+TasneemAhmed@users.noreply.github.com>
2020-07-14 17:40:50 +0200

    Initial commit

---
+++ app/src/main/java/com/androidCourse/dinnerdecider/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.androidCourse.dinnerdecider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private val foodList = arrayListOf("Pizza", "Chicken", "Desert", "Cup Cake")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //whenever the "DECIDE!" button is pressed the between brackets will happen
+
+       decideButton.setOnClickListener{
+           //val random = Random()
+           val randomFood = Random.nextInt(foodList.count()) //get random number between 0 to max of foodList array.
+           selectedFoodTxt.text = foodList[randomFood]  // the text will be the element in foodList array of random index.
+        }
+
+        //whenever the "ADD NEW FOOD..." button is pressed the between brackets will happen
+        addedFoodButton.setOnClickListener{
+           val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()  // this text be cleared after added to foodList arrayList to be ready to inset new food.
+            println(foodList)
+        }
+    }
+}
