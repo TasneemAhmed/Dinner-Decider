a873d0a72ea8950f9590c8fcfc106f2ace1bc1bc
TasneemAhmed <49993791+TasneemAhmed@users.noreply.github.com>
2020-07-16 17:27:20 +0200

    modify age

--- app/src/main/java/com/androidCourse/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/androidCourse/dinnerdecider/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val jop = "Developer"
-
+        val age = 21
         val name = "Tasneem"
         val nameHaccker = "yousef"
 
